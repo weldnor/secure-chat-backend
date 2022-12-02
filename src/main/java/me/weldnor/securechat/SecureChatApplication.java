75f35c1bf1a8f72b24daf8dc589a57ef47d1e673
weldnor <kolesov.anton.s@gmail.com>
2022-12-02 19:05:09 +0300

    Init commit

---
+++ src/main/java/me/weldnor/securechat/SecureChatApplication.java
@@ -0,0 +1,13 @@
+package me.weldnor.securechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/weldnor/securechat/SecureChatApplicationTests.java
@@ -0,0 +1,13 @@
+package me.weldnor.securechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
